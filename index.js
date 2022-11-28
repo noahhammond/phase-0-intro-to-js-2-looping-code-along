71770d70e5e51f4b921e2306a0647f47dce0bada
Noah <business.noah@hotmail.com>
2022-11-28 12:46:32 -0700

    commit tree a8673a9669dc576e1d3f08ae0d9a0b03f8266676

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let thankYou = []
+    for (let i = 0; i < name.length; i++){
+        thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return thankYou
+}
+
+writeCards();
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i--);
+    }
+}
+countDown(10)
